16046499ec6ebec680969e69a797cbc15ecdc7c9
dengle1 <engled93@gmail.com>
2017-10-17 15:15:35 -0400

    Removed uneccessary Code

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,6 +1,6 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name'])  		||
+ if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
    empty($_POST['phone']) 		||
    empty($_POST['message'])	||
